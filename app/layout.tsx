884543d07a350cdcfa5be78049c62600f8e73045
astouafk <deleguedevwebodc@gmail.com>
2026-01-20 14:08:35 +0000

    seo 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,7 +113,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               try {
-                const theme = localStorage.getItem('theme') || 'dark';
+                const theme = localStorage.getItem('theme') || 'light';
                 document.documentElement.classList.add(theme);
               } catch (e) {}
             `,
